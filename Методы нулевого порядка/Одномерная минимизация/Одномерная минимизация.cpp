a8fa30568e8679605fbbb279d6cc943db7acccb6
Yury <bar2104y@yandex.ru>
2020-09-22 23:21:00 +0300

    первая версия

---
+++ Методы нулевого порядка/Одномерная минимизация/Одномерная минимизация.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "fibonachi.h"
+
+
+using namespace std;
+
+
+float parabola(float x)
+{
+	return (x * x + 2 * x + 2);//(x+1)^2 + 1
+}
+
+int main()
+{
+	float e = fibonachi_extr(-10, 10, 0.001, *parabola);
+	cout << "x=" << e << "		y=" << parabola(e);
+	return 0;
+}
