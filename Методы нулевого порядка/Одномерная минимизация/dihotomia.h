23dbf06aa42c3997e7f9a36342a11d57e3401883
Yury <bar2104y@yandex.ru>
2020-09-23 23:59:02 +0300

    add

---
+++ Методы нулевого порядка/Одномерная минимизация/dihotomia.h
@@ -0,0 +1,27 @@
+#pragma once
+
+
+float dih_extr(float a, float b, float e, func f)
+{
+	float l = e;
+	float y, z;
+
+	while (true)
+	{
+		y = (a + b - e) / 2;
+		z = (a + b + e) / 2;
+
+		if (f(y) <= f(z)) b = z;
+		else a = y;
+
+
+		if (abs(b - a) < 2*l)
+			return ((b + a) / 2);
+	}
+		
+
+
+
+	return ((a + b) / 2);
+
+}
