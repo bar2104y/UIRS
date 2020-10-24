bf1b64c55c576171996ec4df25649db23bd512a6
Yury <bar2104y@yandex.ru>
2020-10-24 19:40:03 +0300

    Добавлены новые методы
    Заполнение матрицы, установка столбца,строки, отдельного элемента. Вычисление минора матрицы и определителя.

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -5,17 +5,46 @@
 
 int main()
 {
-    FloatMatrix tmp = FloatMatrix(2, 5);
-    FloatMatrix tmp2 = FloatMatrix(2);
+    
+    //FloatMatrix tmp = FloatMatrix(2, 5);
+    int k = 1;
+    int n = 3;
+    float row [3] = {1.0,2.0,3.0};
+    float** matrix = new float* [n];
+    for (unsigned int i = 0; i < n; i++)
+    {
+        matrix[i] = new float[n];
+        for (unsigned int j = 0; j < n; j++) {
+            matrix[i][j] = k;
+            k++;
+        }
+    }
+    FloatMatrix tmp = FloatMatrix(n, n, matrix);
+    tmp.FillFloat(2.0);
+    tmp.Print();
+    tmp.SetRow(row, 3, 1);
+    tmp.Print();
+    tmp.SetColumn(row, 3, 1);
+    tmp.SetElement(8.0, 1, 1);
+    tmp.Print();
+
+
+    float d = tmp.Det();
+    cout << d << endl;
+
+    /*FloatMatrix tmp = FloatMatrix(3);
+    FloatMatrix tmp2 = FloatMatrix(5);
+    
     tmp.FillFloat(2.0);
     tmp2.FillFloat(3.0);
 
-    FloatMatrix tmp3 = tmp + tmp2;
-
-    tmp3.PrintRow(5);
-    cout << "Uncorrect" << endl;
-    
+    FloatMatrix tmp3 = tmp2 * 2;
+    FloatMatrix tmp4 = (tmp2 + tmp)*2;
+    tmp.Print();
+    tmp2.Print();
     tmp3.Print();
+    tmp4.Print();*/
+
    
     
 }
--- Matrix/matrix.h
+++ Matrix/matrix.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iomanip>
 
+
 using namespace std;
 
 class FloatMatrix
@@ -14,11 +15,21 @@ public:
 	FloatMatrix(unsigned int a, unsigned int b, float** ref);
 	~FloatMatrix();
 
-	void FillFloat(float v);
 
 	void Print();
 	void PrintRow(unsigned int a);
-	//void PrintColumn(unsigned int n);
+	void PrintColumn(unsigned int a);
+
+
+	void FillFloat(float v);
+	void SetRow(float* line, unsigned int length, unsigned int k);
+	void SetColumn(float* column, unsigned int length, unsigned int k);
+	void SetElement(float e, unsigned int a, unsigned int b);
+
+
+	float Minor(unsigned int a, unsigned int b);
+	float Det();
+
 
 	FloatMatrix operator + (FloatMatrix &m2)
 	{
@@ -55,6 +66,43 @@ public:
 				res.matrix[i][j] += m2.matrix[i][j];
 		
 		return FloatMatrix(res.m,res.n, res.matrix);
+
+	}
+	FloatMatrix operator - (FloatMatrix& m2)
+	{
+		unsigned int maxM, maxN, minM, minN;
+		if (this->m > m2.m) {
+			maxM = this->m;
+			minM = m2.m;
+		}
+		else
+		{
+			maxM = m2.m;
+			minM = this->m;
+		}
+
+		if (this->n > m2.n) {
+			maxN = this->n;
+			minN = m2.n;
+		}
+		else
+		{
+			maxN = m2.n;
+			minN = this->n;
+		}
+		// Создать новую матрицу максимальной размерности и в нее класть результат
+
+		FloatMatrix res = FloatMatrix(maxM, maxN);
+
+		for (unsigned int i = 0; i < this->m; i++)
+			for (unsigned int j = 0; j < this->n; j++)
+				res.matrix[i][j] = this->matrix[i][j];
+
+		for (unsigned int i = 0; i < m2.m; i++)
+			for (unsigned int j = 0; j < m2.n; j++)
+				res.matrix[i][j] -= m2.matrix[i][j];
+
+		return FloatMatrix(res.m, res.n, res.matrix);
 
 	}
 	FloatMatrix operator * (int k)
@@ -163,6 +211,24 @@ void FloatMatrix::FillFloat(float v)
 	}
 }
 
+void FloatMatrix::SetRow(float* line, unsigned int length, unsigned int k)
+{
+	if (length != this->n)
+		throw("Uncorrect length");
+
+	for (unsigned int i = 0; i < n; i++)
+		this->matrix[k][i] = line[i];
+}
+
+void FloatMatrix::SetColumn(float* column, unsigned int length, unsigned int k)
+{
+	if (length != this->m)
+		throw("Uncorrect length");
+
+	for (unsigned int i = 0; i < n; i++)
+		this->matrix[i][k] = column[i];
+}
+
 void FloatMatrix::Print()
 {
 	cout << "M: " << this->m << " N: " << this->n<<endl;
@@ -180,9 +246,81 @@ void FloatMatrix::PrintRow(unsigned int a)
 	if (a >= this->m)
 		throw("Ucorrect line");
 
-	cout << "Line: " << this->m << endl;
+	cout << "Line: " << a << endl;
 	for (unsigned int j = 0; j < this->n; j++)
 		cout << fixed << matrix[a][j] << "  ";
 
 	cout << endl; cout << endl;
+}
+
+void FloatMatrix::PrintColumn(unsigned int a)
+{
+	if (a >= this->n)
+		throw("Ucorrect line");
+
+	cout << "Column: " << a << endl;
+	for (unsigned int j = 0; j < this->n; j++)
+		cout << fixed << matrix[j][a] << "  " << endl;
+
+	cout << endl; 
+}
+
+void FloatMatrix::SetElement(float e, unsigned int a, unsigned int b)
+{
+	if (a<0 || b<0 || a>this->m || b>this->n)
+		throw("Uncorrect index");
+	this->matrix[a][b] = e;
+}
+
+
+//Возвращает матрицу размера (m-1;n-1) с вычеркнутый a-той строкой, b-м столбцом
+float FloatMatrix::Minor(unsigned int a, unsigned int b)
+{
+	FloatMatrix res = FloatMatrix(this->m-1, this->n-1);
+	bool o1=0, o2=0;
+	for (unsigned int i = 0; i < m; i++)
+	{
+		if (i == a)
+		{
+			o1 = 1;
+			continue;
+		}
+		o2 = 0;
+		for (unsigned int j = 0; j < n; j++)
+		{
+			if (j == b)
+			{
+				o2 = true;
+				continue;
+			}
+			res.matrix[i - o1][j - o2] = this->matrix[i][j];
+		}
+	}
+	cout << "DETOPR   " << res.Det() << endl;
+	return res.Det();
+
+}
+
+float FloatMatrix::Det()
+{
+	if (m != n)
+		throw("Uncorrect size");
+	float det = 0;
+	if (this->m == 1)
+		return(this->matrix[0][0]);
+	else if (this->m == 2)
+		return((this->matrix[0][0] * this->matrix[1][1]) - (this->matrix[0][1] * this->matrix[1][0]));
+	else
+	{
+		
+		int k = 1;
+		for (unsigned int i = 0; i < this->n; i++)
+		{
+			det += k*this->matrix[0][i] * this->Minor(0, i);
+			k = -k;
+		}
+	}
+	return det;
+	
+	
 }
